ac975c21da6319e244417a07e7c3f6c356c53529
IPGAP\frank.wang <frank.wang@mullenloweprofero.com>
2017-12-28 18:26:02 +0800

    add some js code

--- src/components/ui/user/user.js
+++ src/components/ui/user/user.js
@@ -1,5 +1,20 @@
 require("./user.scss");
 
 module.exports = Elf.Component("user", {
+    
+    onInitial : function () {
+        Elf.attachEvent(window, "onscroll ", this.__animate());
+    },
+
+    onDispose : function () {
+        Elf.detachEvent(window, "onscroll", this.__animate());
+    },
+
+    __animate : function () {
+        var _scrollHeifht = document.body.offsetHeight;
+        var _barHright    = this.refs.nav.height();
+        if (_scrollHeifht)
+    },
+
     render : Elf.redactElement(require("./user.html"))
 });
